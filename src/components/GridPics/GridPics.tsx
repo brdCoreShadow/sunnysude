66422ae52fdb0c9320d7eb17abc2c3e41d850dc7
Oleksandr-Berd <alex.berd86@gmail.com>
2025-08-01 21:15:51 +0300

    project is made

--- src/components/GridPics/GridPics.tsx
+++ src/components/GridPics/GridPics.tsx
@@ -1,24 +1,31 @@
-import * as SC from "./GridPicsStyled"
+import * as SC from "./GridPicsStyled";
 
 import coneMob from "../../assets/images/mobile/image-gallery-cone.jpg";
 import milkMob from "../../assets/images/mobile/image-gallery-milkbottles.jpg";
 import sugarMob from "../../assets/images/mobile/image-gallery-sugar-cubes.jpg";
 import orangeMob from "../../assets/images/mobile/image-gallery-orange.jpg";
+import coneDesk from "../../assets/images/desktop/image-gallery-cone.jpg";
+import milkDesk from "../../assets/images/desktop/image-gallery-milkbottles.jpg";
+import sugarDesk from "../../assets/images/desktop/image-gallery-sugarcubes.jpg";
+import orangeDesk from "../../assets/images/desktop/image-gallery-orange.jpg";
+import { useScreenSize } from "@/hooks/useScreenSize";
 
 const GridPics: React.FC = () => {
+  const { isDesktop } = useScreenSize();
+
   return (
     <SC.GridList>
       <SC.GridItem>
-        <img src={milkMob} alt="milk" />
+        <img src={isDesktop ? milkDesk : milkMob} alt="milk" />
       </SC.GridItem>
       <SC.GridItem>
-        <img src={orangeMob} alt="orange" />
+        <img src={isDesktop ? orangeDesk : orangeMob} alt="orange" />
       </SC.GridItem>
       <SC.GridItem>
-        <img src={coneMob} alt="cone" />
+        <img src={isDesktop ? coneDesk : coneMob} alt="cone" />
       </SC.GridItem>
       <SC.GridItem>
-        <img src={sugarMob} alt="sugar" />
+        <img src={isDesktop ? sugarDesk : sugarMob} alt="sugar" />
       </SC.GridItem>
     </SC.GridList>
   );
